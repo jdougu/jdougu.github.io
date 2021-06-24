5326bd7d87c9dbfb19e5354034a0b411df96ce77
jdudy <jdudy@users.noreply.github.com>
2021-06-24 13:52:38 +1200

    Move filesystem related functions to their own file

--- src/render.tsx
+++ src/render.tsx
@@ -1,9 +1,9 @@
-import { copyFileSync, mkdirSync, readdirSync, writeFileSync } from 'fs';
 import { basename, dirname, extname, join, relative, resolve } from 'path';
 import { JSXInternal } from 'preact/src/jsx';
 import render from 'preact-render-to-string';
 
 import { Html } from './layouts/Html';
+import { copyFile, getFiles, writeFile } from './utils';
 
 const SOURCE_DIR = resolve(process.cwd(), 'src/pages');
 const DEST_DIR = resolve(process.cwd(), 'docs');
@@ -16,24 +16,13 @@ getFiles(SOURCE_DIR).forEach(async (file) => {
         const module = await import(file);
         if (module.default && typeof module.default === 'function') {
             const page = renderPage(module.default);
-            mkdirSync(dirname(destination), { recursive: true });
-            writeFileSync(destination, page);
+            writeFile(destination, page);
         }
     } else if (basename(file)[0] !== '_') {
-        mkdirSync(dirname(destination), { recursive: true });
-        copyFileSync(file, destination);
+        copyFile(file, destination);
     }
 
 });
-
-function getFiles(directory: string): string[] {
-    const dirEntries = readdirSync(directory, { withFileTypes: true });
-    const files = dirEntries.map((dirEntry) => {
-        const path = resolve(directory, dirEntry.name);
-        return dirEntry.isDirectory() ? getFiles(path) : path;
-    });
-    return Array.prototype.concat(...files);
-}
 
 function renderPage(Component: () => JSXInternal.Element) {
     const Page = () => (
---
+++ src/utils/filesystem.ts
@@ -0,0 +1,30 @@
+import { copyFileSync, mkdirSync, readdirSync, writeFileSync } from 'fs';
+import { dirname, resolve } from 'path';
+
+/**
+ * Copy file synchronously, make directories and overwrite if needed.
+ */
+export function copyFile(src: string, dest: string) {
+    mkdirSync(dirname(dest), { recursive: true });
+    copyFileSync(src, dest);
+}
+
+/**
+ * Get a recursive directory listing of absolute paths.
+ */
+export function getFiles(directory: string): string[] {
+    const dirEntries = readdirSync(directory, { withFileTypes: true });
+    const files = dirEntries.map((dirEntry) => {
+        const path = resolve(directory, dirEntry.name);
+        return dirEntry.isDirectory() ? getFiles(path) : path;
+    });
+    return Array.prototype.concat(...files);
+}
+
+/**
+ * Write file synchronously, make directories and overwrite if needed.
+ */
+export function writeFile(dest: string, data: string) {
+    mkdirSync(dirname(dest), { recursive: true });
+    writeFileSync(dest, data);
+}
