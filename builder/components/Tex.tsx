87654f8e543f15248cdcc1279a761c34c3d26340
jdougu <jdougu@users.noreply.github.com>
2023-03-04 14:17:43 +1300

    Initial Tex to MathML code

---
+++ builder/components/Tex.tsx
@@ -0,0 +1,23 @@
+import { KatexOptions, renderToString } from 'katex';
+
+export const $ = (strings: TemplateStringsArray) => {
+    const options: KatexOptions = {
+        displayMode: false,
+        output: 'mathml',
+    };
+    const mathML = renderToString(strings.raw[0], options);
+    return <span dangerouslySetInnerHTML={{ __html: strip(mathML) }} />
+};
+
+function strip(mathML: string) {
+    const spanRegEx = /<[\/]?span[^>]*>/g;
+    mathML = mathML.replace(spanRegEx, '');
+
+    const semanticsRegEx = /<[\/]?semantics[^>]*>/g;
+    mathML = mathML.replace(semanticsRegEx, '');
+
+    const annotationRegEx = /<annotation.*\/annotation>/g;
+    mathML = mathML.replace(annotationRegEx, '');
+
+    return mathML;
+}
